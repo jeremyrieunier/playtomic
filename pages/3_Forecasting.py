5adc1b1b3846862825b1a666643e0513287ed813
Jeremy Rieunier <9092354+jeremyrieunier@users.noreply.github.com>
2025-01-31 10:42:09 +0100

    Update forecasting.py to replace MAU with MTU

--- pages/3_Forecasting.py
+++ pages/3_Forecasting.py
@@ -6,33 +6,33 @@ from prophet import Prophet
 
 gsheet_url = "https://docs.google.com/spreadsheets/d/1ttEndNyIecRf7Arm-lhZrcSIh1vWUmEtTYrCGE9dCSU/edit?gid=69050303#gid=69050303"
 
-# variable to calculate MAU cost
-contract_mau = 1000000
+# variable to calculate MTU cost
+contract_MTU = 1000000
 base_yearly_cost = 5000
 additional_cost = 0.006
 
-# function to calculate MAU cost
-def calculate_cost(total_mau):
-    if total_mau <= contract_mau:
+# function to calculate MTU cost
+def calculate_cost(total_MTU):
+    if total_MTU <= contract_MTU:
         return base_yearly_cost
-    additional_mau = total_mau - contract_mau
-    return round(base_yearly_cost + (additional_mau * additional_cost))
+    additional_MTU = total_MTU - contract_MTU
+    return round(base_yearly_cost + (additional_MTU * additional_cost))
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-mau_data = conn.read(spreadsheet=gsheet_url, usecols=[0, 1])
+MTU_data = conn.read(spreadsheet=gsheet_url, usecols=[0, 1])
 
 # Rename columns for Prophet
-mau_data.columns = ['ds', 'y']
-mau_data['ds'] = pd.to_datetime(mau_data['ds'])
+MTU_data.columns = ['ds', 'y']
+MTU_data['ds'] = pd.to_datetime(MTU_data['ds'])
 
 # Create and fit model
 m = Prophet()
-m.fit(mau_data)
+m.fit(MTU_data)
 future = m.make_future_dataframe(periods=11, freq="M") 
 forecast = m.predict(future)
 
 # Replace January and February 2024 predictions with actual data
-jan_feb_2024_data = mau_data[mau_data['ds'].dt.year == 2024]
+jan_feb_2024_data = MTU_data[MTU_data['ds'].dt.year == 2024]
 forecast.loc[forecast['ds'].isin(jan_feb_2024_data['ds']), 'yhat'] = jan_feb_2024_data['y'].values
 
 # Forecast calculations for 2024
@@ -44,10 +44,10 @@ forecast_2024['yearly_cost'] = forecast_2024['running_total'].apply(calculate_co
 forecast_2024['yearly_cost_lower'] = forecast_2024['running_total_lower'].apply(calculate_cost)
 forecast_2024['yearly_cost_upper'] = forecast_2024['running_total_upper'].apply(calculate_cost)
 
-# MAU orecast calculations for 2024
-final_mau = forecast_2024['running_total'].iloc[-1]
-final_mau_lower = forecast_2024['running_total_lower'].iloc[-1]
-final_mau_upper = forecast_2024['running_total_upper'].iloc[-1]
+# MTU orecast calculations for 2024
+final_MTU = forecast_2024['running_total'].iloc[-1]
+final_MTU_lower = forecast_2024['running_total_lower'].iloc[-1]
+final_MTU_upper = forecast_2024['running_total_upper'].iloc[-1]
 
 # Find when we cross 1M
 crossing_point = forecast_2024[forecast_2024['running_total'] > 1000000].iloc[0]
@@ -69,16 +69,16 @@ additional_cost_upper = forecast_2024['yearly_cost_upper'].iloc[-1]
 
 # Total forecast for 2024
 st.markdown(f"""             
-# Forecast for 2024 accounts for a running total of {final_mau:,.0f} MAU
-- Conservative estimate: {final_mau_lower:,.0f}
-- Optimistic estimate: {final_mau_upper:,.0f}
+# Forecast for 2024 accounts for a running total of {final_MTU:,.0f} MTU
+- Conservative estimate: {final_MTU_lower:,.0f}
+- Optimistic estimate: {final_MTU_upper:,.0f}
 """)
 
 st.markdown(f"""
-### Based on the forecast, we'll reach 1M MAU in:
-- Base scenario: {base_date} with {crossing_point['running_total']:,.0f} MAU
-- Conservative: {lower_date} with {crossing_point_lower['running_total_lower']:,.0f} MAU
-- Optimistic: {upper_date} with {crossing_point_upper['running_total_upper']:,.0f} MAU             
+### Based on the forecast, we'll reach 1M MTU in:
+- Base scenario: {base_date} with {crossing_point['running_total']:,.0f} MTU
+- Conservative: {lower_date} with {crossing_point_lower['running_total_lower']:,.0f} MTU
+- Optimistic: {upper_date} with {crossing_point_upper['running_total_upper']:,.0f} MTU             
 """)
 
 st.line_chart(
@@ -110,4 +110,4 @@ st.markdown("""
 st.dataframe(forecast_2024.round(0))
 
 st.markdown("---")
-st.markdown("Forecast made with Python using [Prophet](https://facebook.github.io/prophet/docs/quick_start.html).")
+st.markdown("Forecast made with Python using [Prophet](https://facebook.github.io/prophet/docs/quick_start.html). Code available on [Github](https://github.com/jeremyrieunier/playtomic/blob/main/pages/3_Forecasting.py).")
